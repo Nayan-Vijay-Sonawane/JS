9f88f324affb131e16c855dd14c37364cd7a7125
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-04 10:48:31 +0000

    numbers and maths

---
+++ 01-basics/06_num_math.js
@@ -0,0 +1,15 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.896;
+
+console.log(otherNumber.toPrecision(4));
+
+const hundreads = 100000000;
+console.log(hundreads.toLocaleString('en-IN'));
