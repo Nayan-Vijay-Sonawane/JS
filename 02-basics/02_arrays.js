8a4b7f159dd0d949fea8f1f70c03e5bb2eefd2ef
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-05 09:14:19 +0000

    New arrays concepts

--- 02-basics/02_arrays.js
+++ 02-basics/02_arrays.js
@@ -9,9 +9,24 @@ const dc_heros = ["Superman", "Batman", "Flash"];
 // marvel_heros.concat(dc_heros);
 // console.log(marvel_heros);
 
-const allHeros = marvel_heros.concat(dc_heros);
-console.log(allHeros);
+// const allHeros = marvel_heros.concat(dc_heros);
+// console.log(allHeros);
 
 // Spread operators
-const newAllHeros = [...marvel_heros, ...dc_heros];
-console.log(newAllHeros);
+// const newAllHeros = [...marvel_heros, ...dc_heros];
+// console.log(newAllHeros);
+
+const another_arr = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+const real_another_arr = another_arr.flat(Infinity);
+console.log(real_another_arr);
+
+console.log(Array.isArray("Nayan"));
+console.log(Array.from("Nayan"));
+console.log(Array.from({name: "Nayan"})); // have to declare from which we have to create the array like from the key or from the value.
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
