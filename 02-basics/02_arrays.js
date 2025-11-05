ae82646ef8643c05f45e30e866760598c76ab938
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-05 09:07:48 +0000

    new arrays concepts

--- 02-basics/02_arrays.js
+++ 02-basics/02_arrays.js
@@ -1,11 +1,17 @@
 const marvel_heros = ["Ironman", "Thor", "Hulk"];
 const dc_heros = ["Superman", "Batman", "Flash"];
 
-marvel_heros.push(dc_heros);
+// marvel_heros.push(dc_heros);
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][2]);
 
-marvel_heros.concat(dc_heros);
-console.log(marvel_heros);
+// marvel_heros.concat(dc_heros);
+// console.log(marvel_heros);
 
+const allHeros = marvel_heros.concat(dc_heros);
+console.log(allHeros);
+
+// Spread operators
+const newAllHeros = [...marvel_heros, ...dc_heros];
+console.log(newAllHeros);
