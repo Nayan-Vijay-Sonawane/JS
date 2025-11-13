ea14117660bcbfb5798062faf5d542ec47b765d9
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-13 14:27:04 +0000

    new objects concept

--- 02-basics/03_objects.js
+++ 02-basics/03_objects.js
@@ -3,4 +3,13 @@
 // Object literals
 
 //Object.create // constructor method in which singleton is created
-const JsUser = {}
+const JsUser = {
+    name: "Nayan",
+    age: 18,
+    email: "nayansonawane@google.com",
+    location: "Mumbai",
+    isLoggedIn : false, 
+    lastLoginDays: ["Monday", "Wednesday"]
+}
+
+console.log(JsUser.email);
