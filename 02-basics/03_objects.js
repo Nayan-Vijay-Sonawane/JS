6027cdec14297da71c9d737f0f0896cc129c656a
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-13 14:36:57 +0000

    new concepts of objects

--- 02-basics/03_objects.js
+++ 02-basics/03_objects.js
@@ -2,10 +2,13 @@
 
 // Object literals
 
+const mySym = Symbol("Key1");
+
 //Object.create // constructor method in which singleton is created
 const JsUser = {
     name: "Nayan",
     age: 18,
+    [mySym]: "MyKey1",
     email: "nayansonawane@google.com",
     location: "Mumbai",
     isLoggedIn : false, 
@@ -13,3 +16,12 @@ const JsUser = {
 }
 
 console.log(JsUser.email);
+console.log(JsUser["location"]);
+console.log(typeof(JsUser[mySym]))
+
+JsUser.email = "nayansonawane@chatgpt.com";
+console.log(JsUser["email"]);
+
+Object.freeze(JsUser);
+JsUser.email = "nayansonawane@mircosoft.com";
+console.log(JsUser);
