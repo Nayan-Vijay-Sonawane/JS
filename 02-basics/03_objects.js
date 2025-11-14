ca06200f74bdd578c4ab1c6428c5374d49ad52e6
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-14 06:40:01 +0000

    new concepts in objects

--- 02-basics/03_objects.js
+++ 02-basics/03_objects.js
@@ -15,14 +15,27 @@ const JsUser = {
     lastLoginDays: ["Monday", "Wednesday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["location"]);
+// console.log(JsUser.email);
+// console.log(JsUser["location"]);
 
-console.log(typeof(JsUser[mySym]));     // If we have to refer to the symbol, so we have to put it in the square brackets
+// console.log(typeof(JsUser[mySym]));     // If we have to refer to the symbol, so we have to put it in the square brackets
 
 JsUser.email = "nayansonawane@chatgpt.com";
-console.log(JsUser["email"]);
+// console.log(JsUser["email"]);
 
-Object.freeze(JsUser);
+// Object.freeze(JsUser);
 JsUser.email = "nayansonawane@mircosoft.com";
-console.log(JsUser);
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS Users");
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greeting);   // It gives function reference
+
+JsUser.greetingTwo = function() {
+    console.log(`Hello JS User, ${this.name} this side.`);
+}
+
+console.log(JsUser.greetingTwo());
