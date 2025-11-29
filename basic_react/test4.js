1b2775082617950c9d96e9445b265c4c67eb67b5
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-29 13:51:51 +0000

    new concept

--- basic_react/test4.js
+++ basic_react/test4.js
@@ -5,3 +5,17 @@
 - will use map method to displaying the list of data.
 
 */
+
+const products = ["product1", "product2", "product3"];
+
+const displayItems = products.map(function (product) {
+    return "<li>" + product + "</li>";
+});
+
+// With arrow function
+
+// const displayItems = products.map((product) => {
+//     return "<li>" + product + "</li>";
+// });
+
+console.log(displayItems);
