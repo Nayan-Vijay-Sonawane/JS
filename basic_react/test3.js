f0045c3ea98aca8df7afe161cb48f00dff443586
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-29 12:39:54 +0000

    new

--- basic_react/test3.js
+++ basic_react/test3.js
@@ -8,3 +8,24 @@ const user = {
 // Accessing Objects
 console.log(user.name);
 console.log(user["email"]);
+
+
+const newUSer = {
+    name: "Sam",
+    email: "sam@yahoo.com",
+    age: 35,
+    country: "US"
+}
+
+// if we have multiple properties then we have to declare multiple properties
+// const name = newUSer["name"];
+// const email = newUSer["email"];
+// const age = newUSer["age"];
+// const country = newUSer["country"];
+
+
+// To solve that issue we have Object destructuring
+const {name: userName, email, age, country} = newUSer;    // to change name of variable 
+
+
+console.log(userName, email, age, country);
