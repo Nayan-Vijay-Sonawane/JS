25a9d1bc582a809702b0521cc8b1ca06685b24d0
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-29 18:47:45 +0000

    new array map method

--- basic_react/test4.js
+++ basic_react/test4.js
@@ -9,14 +9,14 @@
 const products = ["product1", "product2", "product3"];  
 
 // map method returns new array will not change the previous array
-const displayItems = products.map(function (product) {
-    return "<li>" + product + "</li>";
-});
+// const displayItems = products.map(function (product) {
+//     return "<li>" + product + "</li>";
+// });
 
 // With arrow function
 
-// const displayItems = products.map((product) => {
-//     return "<li>" + product + "</li>";
-// });
+const displayItems = products.map((product) => 
+    `<li> ${product} </li>`
+);
 
 console.log(displayItems);
