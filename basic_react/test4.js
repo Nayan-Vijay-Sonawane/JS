26218bda839342088a389ebf0cca1934a4adb754
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-29 18:42:36 +0000

    new array method concep

--- basic_react/test4.js
+++ basic_react/test4.js
@@ -6,8 +6,9 @@
 
 */
 
-const products = ["product1", "product2", "product3"];
+const products = ["product1", "product2", "product3"];  
 
+// map method returns new array will not change the previous array
 const displayItems = products.map(function (product) {
     return "<li>" + product + "</li>";
 });
