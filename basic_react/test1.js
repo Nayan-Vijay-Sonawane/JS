bdc4597b7b28212ff52dd266298941f3626ba90a
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-29 12:18:37 +0000

    new concepts of arrow functions

--- basic_react/test1.js
+++ basic_react/test1.js
@@ -1,7 +1,16 @@
 function printloop() {
+    // Function scope
     for(var i=0; i<3; i++){
         console.log(i);
     }
+    console.log("OUTSIDE", i);
+
+    // Block scope
+    for(let j=0; j<5; j++){
+        console.log(j);
+    }
+
+    // console.log("OUT", j);   // j is not defined
 }
 
 printloop();
---
+++ basic_react/test2.js
@@ -0,0 +1,17 @@
+/* 
+- Arrow fun is another way to define JavaScript functions.
+- Can write JS functions in simple and easy way.
+- Its useful when working on advanced topics
+
+*/
+
+function hello() {
+    console.log("Hello New to JavaScript!");
+}
+
+const greet = () => {
+    console.log("Hello New World of Javascript!");   
+}
+
+hello();
+greet();
