dc17449706372c8366911e44b101d40b23c318c7
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-29 19:10:28 +0000

    ternary operator

--- basic_es6/test7.js
+++ basic_es6/test7.js
@@ -1,5 +1,10 @@
 /*
 - Ternary operator is used for adding the condition.
 - shortcut way of writing if-else condition.
-
+condition ? trueExpression : falseExpression
 */
+
+const marks = 50;
+
+const result = marks > 35 ? "Pass" : "Fail";
+console.log(result);
