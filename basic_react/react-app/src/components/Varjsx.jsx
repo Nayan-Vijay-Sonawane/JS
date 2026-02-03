022baebdbbc9f7fb80862b4eb06e95bfc0551386
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-03 12:31:24 +0000

    new react app

---
+++ basic_react/react-app/src/components/Varjsx.jsx
@@ -0,0 +1,13 @@
+function Varjsx() {
+    const name = "Nayan";
+    const age = 25;
+
+    return(
+        <div>
+            <h1>Hello {name}</h1>
+            <p>Age: {age}</p>
+        </div>
+    )
+}
+
+export default Varjsx;
