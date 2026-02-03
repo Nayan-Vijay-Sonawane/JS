e0df5b31c71a4a2772f627437502464316bbcc9f
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-03 08:37:40 +0000

    new react app

--- basic_react/react-app/src/App.jsx
+++ basic_react/react-app/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
+    <div>
     <h1> Hello React</h1>
+      <Header/>
+      <Footer/>
+    </div>
   )
 }
 
