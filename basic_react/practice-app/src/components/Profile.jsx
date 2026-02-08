ca037dd40adaf7cef4e27346fafaa3999269023b
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-08 14:57:47 +0000

    new react application

--- basic_react/practice-app/src/components/Profile.jsx
+++ basic_react/practice-app/src/components/Profile.jsx
@@ -11,11 +11,11 @@
 
 // Props with Destructuring (Professional Way)
 
-function Profile({name, role}) {
+function Profile({props}) {
     return (
         <div>
-            <h2>{name}</h2>
-            <p>{role}</p>
+            <h2>{props.name}</h2>
+            <p>{props.role}</p>
         </div>
     )
 }
