15887c920ab44d1fa2cffc295223a2535778c33b
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-12 10:05:23 +0000

    Rendering Application

--- basic_react/rendering-app/src/components/Dependency.jsx
+++ basic_react/rendering-app/src/components/Dependency.jsx
@@ -1,10 +1,19 @@
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
 
 function Dependency() {
 
+    const [count, setCount] = useState(0);
+
     useEffect(() => {
         console.log("Mounted");
-    }, [])
+    }, [count])
+
+    return(
+        <>
+        <h1>This is the counter app</h1>
+        <button onClick={() => {setCount(count + 1)}}>Count : {count}</button>
+        </>
+    )
 }
 
 export default Dependency;
