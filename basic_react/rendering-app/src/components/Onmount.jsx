702e87d4304a3566a0f0012da5e0af9bc70fd586
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-12 11:55:59 +0000

    Rendering Application

--- basic_react/rendering-app/src/components/Onmount.jsx
+++ basic_react/rendering-app/src/components/Onmount.jsx
@@ -1,20 +1,21 @@
 import {useState, useEffect} from 'react';
+import localData from './dataone.json';
 
 function Onmount() {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
+        fetch("https://jsonplaceholder.typicode.com/todos/1")
             .then(res => res.json())
-            .then(data => setUsers(data));
+            .then(dataone => setUsers(dataone));
     }, []);
 
     return(
         <div>
             <h2>User List</h2>
-            {users.map(user => {
+            {users.map(user => (
                 <p key={user.id}>{user.name}</p>
-            })}
+            ))}
         </div>
     )
 
