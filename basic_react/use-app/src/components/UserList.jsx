49f1dbf6e270c63ed035a62b570fb636bda9a286
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-13 12:33:55 +0000

    User Listing

--- basic_react/use-app/src/components/UserList.jsx
+++ basic_react/use-app/src/components/UserList.jsx
@@ -4,4 +4,12 @@ function UserList() {
     const [users, setUser] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    // function to fetch users
+    const fetchUsers = async() => {
+        try{
+            setLoading(false);
+            setError(null);
+        }
+    }
 }
