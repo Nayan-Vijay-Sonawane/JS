9dcc50db5cb09974dc86e0c3f60f1950614c89e7
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-13 12:30:13 +0000

    User Listing

--- basic_react/use-app/src/components/UserList.jsx
+++ basic_react/use-app/src/components/UserList.jsx
@@ -1,5 +1,7 @@
 import {useState, useEffect} from 'react';
 
 function UserList() {
-    
+    const [users, setUser] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 }
