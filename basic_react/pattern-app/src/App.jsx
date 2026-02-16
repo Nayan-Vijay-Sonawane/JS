ba0e1116bab960abd030e623ff3102f73a2c7963
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-16 09:25:00 +0000

    Adding the Counter Buttons

--- basic_react/pattern-app/src/App.jsx
+++ basic_react/pattern-app/src/App.jsx
@@ -1,12 +1,19 @@
+import {useState} from 'react';
 import './App.css'
+import CounterDisplay from './components/CounterDisplay';
+import CounterButtons from './components/CounterButtons';
 
 function App() {
 
+  const [count, setCount] = useState(0);
+  console.log("App rendered");
+
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <CounterDisplay count={count}/>
+      <CounterButtons count={count} setCount={setCount}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
