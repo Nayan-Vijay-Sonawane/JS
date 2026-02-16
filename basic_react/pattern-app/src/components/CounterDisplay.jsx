a46e378e17094eb68b61f5a7ddb19b9374a2319f
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-16 09:14:22 +0000

    Adding CounterDisplay

---
+++ basic_react/pattern-app/src/components/CounterDisplay.jsx
@@ -0,0 +1,14 @@
+
+function CounterDisplay({count}) {
+    
+    console.log("Counter Display rendered");
+
+    return(
+        <div>
+            <h1>Current Count: {count}</h1>
+        </div>
+    )
+
+}
+
+export default CounterDisplay;
