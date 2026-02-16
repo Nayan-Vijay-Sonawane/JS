00b1aa6c16f5432f6cab5f8fc31b0ac3e5cdd560
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-16 09:21:58 +0000

    Adding Counter Buttons

---
+++ basic_react/pattern-app/src/components/CounterButtons.jsx
@@ -0,0 +1,15 @@
+// Modifying the count only
+
+function CounterButtons({count, setCount}) {
+    console.log("CounterButtons rendered");
+
+    return(
+
+        <div>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+    );
+} 
+
