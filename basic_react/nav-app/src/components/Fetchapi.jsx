0367404a55e0e5abf8eb7982e8a0e5d7fa9a1afd
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-23 16:11:08 +0000

    API Integration using Async Await

---
+++ basic_react/nav-app/src/components/Fetchapi.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+function Fetchapi() {
+
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const res = await fetch("API_URL");
+                const data = await res.json();
+                setData(data);
+            }
+            catch(err){
+                setError("Failed to fetch");
+            }
+        };
+
+        fetchData();
+    }, [])
+
+    return(
+        <div>
+            <h2>API Data Fetch Example</h2>
+            {error && <p>{error}</p>}
+            {data && <p>{data}</p>}
+        </div>
+    )
+}
+
+export default Fetchapi;
