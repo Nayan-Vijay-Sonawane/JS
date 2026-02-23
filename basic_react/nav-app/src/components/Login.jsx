773f7abdb61d00fbcd3728f569448c2d1f670636
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-02-23 15:36:34 +0000

    Adding Forms

--- basic_react/nav-app/src/components/Login.jsx
+++ basic_react/nav-app/src/components/Login.jsx
@@ -2,7 +2,16 @@ import {useState} from 'react';
 
 function Login(){
     const [email, setEmail] = useState("");
-    const [error, setError] = 
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!email.includes("@")){
+            setError("Invalid Email");
+            return;
+        }
+    }
 }
 
 export default Login;
