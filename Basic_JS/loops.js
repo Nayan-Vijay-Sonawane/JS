306f2a47e9e59b94cc4c3762554a3401843bff4f
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-01 13:50:17 +0000

    New Loops are added

--- Basic_JS/loops.js
+++ Basic_JS/loops.js
@@ -2,7 +2,14 @@
 // for, while, for...of, forEach
 
 // print 1-100
-
+// for loop
 for(i=1; i<=100; i++){
     console.log(i);
+}
+
+// while loop
+let j=1;
+while(j<=100){
+    console.log(j);
+    j++;
 }
