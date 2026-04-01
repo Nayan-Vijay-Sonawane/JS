728b7ff858a6a0d4d863595f9a22c55d21f120e2
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-01 13:08:50 +0000

    New Variables and Data types added

--- Basic_JS/vardata.js
+++ Basic_JS/vardata.js
@@ -8,3 +8,29 @@ console.log(name);
 console.log(age);
 console.log(isLoggedIn);
 
+// Print type using typeof
+
+// Type conversion(Explicit) and type coercison(Implicit)
+console.log("Explicit Conversion");
+let a = 5;
+let b = String(a);
+console.log(b);
+console.log(typeof(a));
+console.log(typeof(b));
+
+console.log("Implicit Conversion");
+let n = 1;
+let s = "45";
+let res = n + s;    // converts num to string
+console.log(res);
+console.log(typeof(res));
+
+
+// Create Object for a User
+
+const emp = {
+    name: "Nayan",
+    age: 25,
+    EmpID: 1234,
+    
+}
