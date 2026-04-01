ebb31d47bc0d8e37717be945e2b2dd24e50c1f5e
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-01 14:59:45 +0000

    New Functions are added

--- Basic_JS/newfunctions.js
+++ Basic_JS/newfunctions.js
@@ -1,3 +1,15 @@
 // Functions
 // functions are just a reusable blocks of code designed to perform a specific task.
 
+// Function Statment or Function Declaration
+function a() {
+    console.log("a called");
+};
+
+// Function Expression
+// Assign a function to variable
+const b = function(){
+    console.log("b called");
+};
+
+// Anonymous Function
