2c8e9d9659a108b85433d34fd70ec66b1fde1cdd
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-02 14:18:40 +0000

    New Function is added which gets factorial of a number

--- Basic_JS/newfunctions.js
+++ Basic_JS/newfunctions.js
@@ -131,3 +131,18 @@ function isPal(str){
 }
 
 console.log(isPal("Level"));
+
+// Factorial
+function factorial(n){
+    if(n<0){
+        return "Invalid number";
+    }
+    
+    let result = 1;
+    for(let i=1; i <= n; i++){
+        result = result * i;
+    }
+    return result;
+}
+
+console.log(factorial(5));
