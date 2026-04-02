0a2ab15a5a8072064c801e85659df6faecee9e4c
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-02 14:30:37 +0000

    New Function which checks the maximum number in the array

--- Basic_JS/arrobj.js
+++ Basic_JS/arrobj.js
@@ -15,3 +15,17 @@ const emp = {
 }
 
 console.log(emp);
+
+// Find maximum in array
+function findMax(arr){
+    let max = arr[0];
+
+    for(let i=0; i <= arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(findMax([3, 2, 5, 6, 8, 3, 9, 1]));
