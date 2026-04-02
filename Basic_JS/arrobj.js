89b1760131ee277ba9f8193c13afb3e42c9c5b7d
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-02 14:26:23 +0000

    New Obects are added

--- Basic_JS/arrobj.js
+++ Basic_JS/arrobj.js
@@ -3,3 +3,15 @@
 
 const names = ["HTML", "CSS", "JAVASCRIPT", "REACT", "NODE", "EXPRESS"];
 console.log(names);
+
+// Objects
+// A collection of data and functionality which is stored as key value pair
+const emp = {
+    name: "Nayan",
+    age: 25,
+    email: "nayan@google.com",
+    empID: 1234,
+    loaction: "Mumbai"
+}
+
+console.log(emp);
