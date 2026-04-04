ff133572f7ed571da9ed91647b2a8829e194bfd0
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-04 12:13:45 +0000

    New Function to remove the duplicates from the array

--- Basic_JS/Practice-1/remdup.js
+++ Basic_JS/Practice-1/remdup.js
@@ -1,1 +1,10 @@
 // Remove duplicates from the array
+
+function remduplicate(){
+    const num = [2, 3, 4, 6 ,3, 6, 7, 8, 9];
+    const check = new Set(num);
+    console.log(check);
+}
+
+remduplicate();
+// console.log(remduplicate());
