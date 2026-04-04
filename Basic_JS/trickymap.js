9d210031a64ff32f32f5efd0ddfff69ee3687ee9
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-04 18:24:48 +0000

    New Function to get the list with full names

---
+++ Basic_JS/trickymap.js
@@ -0,0 +1,10 @@
+// Find the list with full names
+//["Nayan Sonawane", "Donald Trump", ...]
+
+const users = [
+    { firstName: "Nayan", lastName: "Sonawane", age: 25},
+    { firstName: "Donald", lastName: "Trump", age: 75},
+    { firstName: "Elon", lastName: "Musk", age: 52},
+    { firstName: "Jeff", lastName: "Bezos", age: 70},
+];
+
