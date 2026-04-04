8d8c7b1413b6b731b6a9b2c68e0746930fe09bc8
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-04 15:40:23 +0000

    Map method

--- Basic_JS/arrmethods.js
+++ Basic_JS/arrmethods.js
@@ -1,3 +1,14 @@
 // Array methods
 // Map method
 // map() creates a new array by applying a function to each element
+
+const arr = [2, 3, 1, 4, 7, 9];
+
+function double(x){
+    return x * 2;
+}
+const output = arr.map(double);
+const binary = arr.map((x) => x.toString(2));
+
+console.log(output);
+console.log(binary);
