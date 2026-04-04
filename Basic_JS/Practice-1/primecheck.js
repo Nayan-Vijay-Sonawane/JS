354281ca74e3a5ec728ae5224b7633246e284628
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-04 12:23:06 +0000

    New Function to check the number is prime or odd

---
+++ Basic_JS/Practice-1/primecheck.js
@@ -0,0 +1,13 @@
+// To check the number is prime or odd
+
+function prime(n){
+    if(n % 2 == 0){
+        console.log(n + " the number is Prime");
+    }
+    else{
+        console.log(n + " the number is Odd");
+    }
+}
+
+prime(15);
+// console.log(prime(15));
