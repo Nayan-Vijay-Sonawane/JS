127181ff8ab4d828d114c316920c9fde91486e77
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-05 15:00:15 +0000

    New Function of Sync and Async Check

--- Basic_JS/asynch.js
+++ Basic_JS/asynch.js
@@ -1,2 +1,32 @@
 // Asynchronous Javascript
-// 
+
+function checkInventory(){
+    setTimeout(() => {
+        console.log("Checking the Inventory...");
+    }, 2000)
+}
+
+function createOrder(){
+    setTimeout(() => {
+        console.log("Checking the Order...");
+    }, 2000)
+}
+
+function chargePayment(){
+    setTimeout(() => {
+        console.log("Charging the Payment...");
+    }, 2000)
+}
+
+function sendInvoice(){
+    setTimeout(() => {
+        console.log("Sending the Invoice...");
+    }, 2000);
+}
+
+function main(){
+    checkInventory();
+    createOrder();
+    chargePayment();
+    sendInvoice();
+}
