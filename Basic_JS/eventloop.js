fe81ceb521f82cc4f58ed5dbaa27b970a9abccdf
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-08 14:35:02 +0000

    Checking the new concept closure in the event loops

--- Basic_JS/eventloop.js
+++ Basic_JS/eventloop.js
@@ -1,6 +1,6 @@
 // Hoisting 
 
-console.log(y);
+// console.log(y);
 console.log(x);
 greet();
 
@@ -9,3 +9,23 @@ const y = 20;   // const and let are hoisted with but remain in the temporal dea
 function greet(){
     console.log("Greeting...");
 }
+
+
+// Closure
+// A closure is when a function remembers variables from the outer scope, even after that outer 
+// function has finished executing.
+function outer() {
+    let count = 0;
+
+    function inner(){
+        count++;
+        console.log(count);
+    }
+    return inner;
+}
+const myFun = outer();
+
+myFun();
+myFun();
+myFun();
+
