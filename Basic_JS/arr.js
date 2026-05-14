7afc29f1893682a020fcebb5e6bfc4dfae3428eb
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-14 12:12:40 +0000

    New Information is added

--- Basic_JS/arr.js
+++ Basic_JS/arr.js
@@ -18,9 +18,36 @@ console.log(brr.slice(2, 4));       // new part
 brr.splice(1, 2, 'kunal')            // adds or removes element from an specific index
 
 console.log(brr);
+
 // map() creates a new array by applying a function to each element
 const arr = [10, 20, 30];
 const ansArray = arr.map((num) => {
     return num*num;
 })
 console.log(ansArray);
+
+// filter() creates a new array with elements that pass a condition.
+
+const eve = [10, 20, 30, 11, 21, 44, 51,'Nayan', 'New']
+const evenArray = eve.filter((num) => {
+    return typeof(num) === 'string';
+    // return num%2===0;
+    // if(num%2===0){
+    //     return true;
+    // }
+    // else{
+    //     return false;
+    // }
+});
+
+console.log(evenArray);
+
+// reduce() reduces an array to a single value
+
+const newA = [10, 20, 30, 40];
+
+const ans = newA.reduce((acc, curr) => {
+    return acc + curr;
+}, 0);
+
+console.log(ans);
