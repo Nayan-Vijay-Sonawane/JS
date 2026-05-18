66c197b5c1477481fa9944a9780c8392ad2ba7e4
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-18 08:34:32 +0000

    Window object

---
+++ Basic_JS/dom1.js
@@ -0,0 +1,4 @@
+// Window: 
+// Its a global object we can access it from anywhere it doesn't have any scope
+// It is created by browser and it represent browser window
+// window = DOM, BOM, JS Core(functionalities)
