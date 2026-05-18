37df2df2c88f8ae2b7ddcdfd6c1c22c6944dbcec
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-18 08:40:41 +0000

    Document Object Model

--- Basic_JS/dom1.js
+++ Basic_JS/dom1.js
@@ -2,3 +2,10 @@
 // Its a global object we can access it from anywhere it doesn't have any scope
 // It is created by browser and it represent browser window
 // window = DOM, BOM, JS Core(functionalities)
+
+// DOM:
+// Document Object Model
+// for eg we converted the entire code in JS object so we called that a document
+
+// BOM: 
+// It allows JS to talk to browser about matter other than content of page.
