637cb523fd52bd234ffc625e7403accaab316d7b
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-18 08:44:46 +0000

    Document Object Model

--- Basic_JS/dom1.js
+++ Basic_JS/dom1.js
@@ -3,9 +3,9 @@
 // It is created by browser and it represent browser window
 // window = DOM, BOM, JS Core(functionalities)
 
+// BOM: 
+// It allows JS to talk to browser about matter other than content of page.
+
 // DOM:
 // Document Object Model
 // for eg we converted the entire code in JS object so we called that a document
-
-// BOM: 
-// It allows JS to talk to browser about matter other than content of page.
