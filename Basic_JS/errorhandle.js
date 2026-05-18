f28341671568bee765aaf6582fae60a6f42442f3
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-18 08:13:43 +0000

    New Error handling concept

--- Basic_JS/errorhandle.js
+++ Basic_JS/errorhandle.js
@@ -1,3 +1,16 @@
 // Error Handling 
 //  try-catch
 // If try block may have errors then catch block is executed
+function fullName(value){
+    if(typeof value !== String){
+        throw new Error("Its not a string");
+    }
+    return value;
+};
+
+try{
+    console.log(fullName("Naan"));
+}
+catch(e){
+    console.log(e.message);
+}
