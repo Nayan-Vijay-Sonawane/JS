141ef2279b7b502b59f391d7c54447a2b9bf23b3
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-21 07:17:46 +0000

    New events concepts

--- Basic_JS/events.js
+++ Basic_JS/events.js
@@ -9,3 +9,16 @@
 // Events
 // Events are nothing but the actions or occurrences happen in the browser.
 // eg. click, key press, scroll, etc.
+
+// for eg
+{/*
+    we have a page in that page we have a button, when we click on the button some video is playing
+
+    event = 'click'
+    event-target = 'button'
+    video run/play = 'action/behaviour'
+    we write this in the event-listner
+
+    To handle the events what actions need to take that we can write in the event-listner.
+
+*/}
