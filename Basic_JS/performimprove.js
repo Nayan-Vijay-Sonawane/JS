964a6e4f37891bf2daeae1370a0a78aa71deaa36
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-22 09:48:16 +0000

    New concepts of performance improvment

--- Basic_JS/performimprove.js
+++ Basic_JS/performimprove.js
@@ -1,3 +1,15 @@
 // Performance Improvement
+// The method is a part of time related performance information
 
+// code 1
 
+const t1 = performance.now();
+for(let i=0; i<=100; i++){
+    let para = document.createElement('p');
+    para.textContent = "This is the para: " + i;
+    document.body.appendChild(para);
+}
+
+const t2 = performance.now();
+
+console.log("total time taken by code 1: " + (t2-t1));
