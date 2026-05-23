3230c9ff20012e32815ffd3009a31526480f4531
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-23 16:58:56 +0000

    New concepts of event loop with starting synchronous code

--- Basic_JS/eventloop2.js
+++ Basic_JS/eventloop2.js
@@ -2,6 +2,8 @@
 
 // Synchronous code
 // Js is single threaded language so a single request is executed at one time
+// until the first request gets finished, the second request won't be touched
+// 
 console.log("Hi");
 console.log("Nayan");
 console.log("Sonawane")
