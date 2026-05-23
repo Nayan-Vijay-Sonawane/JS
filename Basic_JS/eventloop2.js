973b0d6c53b6af9073be95db352f4502acd50911
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-23 16:57:42 +0000

    New concepts of event loop

--- Basic_JS/eventloop2.js
+++ Basic_JS/eventloop2.js
@@ -1,7 +1,7 @@
 // Event loop
 
 // Synchronous code
-// Js is single threaded language
+// Js is single threaded language so a single request is executed at one time
 console.log("Hi");
 console.log("Nayan");
 console.log("Sonawane")
