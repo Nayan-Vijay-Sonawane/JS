4ae8ceba4e3a47b84aed5171ac7fd4240449b5c2
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-25 16:38:43 +0000

    New concepts of event loop with Asynchronous JS

--- Basic_JS/eventloop2.js
+++ Basic_JS/eventloop2.js
@@ -29,4 +29,4 @@ console.log("This request never gets on call stack");
 
 // Asynchronous code
 // Event loop
-// This is a mechanism of JS that
+// This is a mechanism of JS that allows the runtime evnironment to manage the async
