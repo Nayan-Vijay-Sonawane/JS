689766f7d1b9a1129d7cb1d3fe40cea2c91fbf09
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-29 14:25:10 +0000

    New callback function for async code

--- Basic_JS/01callbacks.js
+++ Basic_JS/01callbacks.js
@@ -9,3 +9,35 @@ setTimeout(() => {
 }, 2000);
 
 console.log("End");
+
+// async callbacks 
+
+function checkInventory(){
+    setTimeout(() => {
+        console.log("Checking the Inventory...");
+    }, 2000);
+}
+
+function createOrder(){
+    setTimeout(() => {
+        console.log("Creating the Order...");
+    }, 1000);
+}
+
+function chargePayment(){
+    setTimeout(() => {
+        console.log("Charging the Payment...")
+    }, 2000);
+}
+
+function sendInvoice(){
+    setTimeout(() => {
+        console.log("Sending the Invoice...");
+    }, 1000);
+}
+
+function main(){
+
+}
+
+main();
