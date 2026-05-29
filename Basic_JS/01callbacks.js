b25107f16a313b77da5599c3f4dfc5967e1c13ba
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-29 14:08:11 +0000

    New callback function for async code

--- Basic_JS/01callbacks.js
+++ Basic_JS/01callbacks.js
@@ -1,2 +1,11 @@
 // Callback
 // A function that is passed as an argument to another function and executed later
+// callback function can execute after another function has finished
+
+console.log("Start");
+
+setTimeout(() => {
+    console.log("Inside setTimeout");
+}, 2000);
+
+console.log("End");
