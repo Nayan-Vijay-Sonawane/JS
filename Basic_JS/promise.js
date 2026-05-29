1135b8225d06f158f713c6f9d33928b20aafa8e3
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-29 13:22:53 +0000

    New concept promises with three states

--- Basic_JS/promise.js
+++ Basic_JS/promise.js
@@ -2,5 +2,5 @@
 // A promise is an object represents the eventual completion of an asynchronous operation and its resulting value.
 // promise has three states
 // Pending   -> Initial state neither fulfilled nor rejected
-// Fulfilled ->
-// Rejected  ->
+// Fulfilled -> The operation completed successfully
+// Rejected  -> The operation is failed
