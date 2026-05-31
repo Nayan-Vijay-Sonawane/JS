e76d3df3b66608ddadc69244ddd02152ad597637
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-05-31 13:57:19 +0000

    New concept of async/await to handle the promises in more concise way

--- Basic_JS/03asyncawait.js
+++ Basic_JS/03asyncawait.js
@@ -1,5 +1,8 @@
 // Async await
 // async/await provides more concise and readable way to work with promises, making asynchronous code appears synchronous
 // async -> makes a function to return a Promise
-// await -> wait for a promise to finish
+// await -> waits for a promise to finish
+// await can only be used inside an async function
+// Promise fulfilled -> returns value
+// Promise rejected -> goes to catch block
 // 
