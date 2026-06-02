12a7decda074ae03166c4844a919304ecceb2931
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-02 08:01:51 +0000

    New concept fetch API with async/await

--- Basic_JS/04fetch.js
+++ Basic_JS/04fetch.js
@@ -1,3 +1,10 @@
 // Fetch API
 // The fetch API provides an interface for fetching the resources. It is more powerful and flexible replacement for
 // XMLHttpRequest
+
+
+// scenario
+// prepare url / api endpoint
+// await // fetch data -> network call -> async
+// process data -> sync
+
