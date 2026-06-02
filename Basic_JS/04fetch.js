0748e50dd153a3b0e5cae9e5e1dd1c6bf922282f
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-02 08:06:06 +0000

    New concept fetch API with async/await

--- Basic_JS/04fetch.js
+++ Basic_JS/04fetch.js
@@ -8,3 +8,12 @@
 // await // fetch data -> network call -> async
 // process data -> sync
 
+async function getData(){
+
+    // get request -> async
+    let response = fetch('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(response);
+};
+
+getData();
+
