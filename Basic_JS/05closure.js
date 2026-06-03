46013f9cd0cabf80d53f410c6dd2ba4ed435fb32
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-03 09:42:26 +0000

    New concept closure

--- Basic_JS/05closure.js
+++ Basic_JS/05closure.js
@@ -1,2 +1,4 @@
 // Closure
-// A closure is when a function remembers variables from the outer scope even after that outer function has finished executing.
+// A closure is when a function remembers variables from the outer scope even after that outer 
+// function has finished executing.
+
