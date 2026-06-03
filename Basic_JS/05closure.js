67fc511a8fb37623408fc4751497c0f8b6ef3403
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-03 09:47:47 +0000

    New concept Closure

--- Basic_JS/05closure.js
+++ Basic_JS/05closure.js
@@ -2,3 +2,13 @@
 // A closure is when a function remembers variables from the outer scope even after that outer 
 // function has finished executing.
 
+function outerFunction(){
+    let username = "Nayan";
+    function innerFunction(){
+        let password = "nayan1234";
+        console.log(`${username} has password ${password}`);
+    };
+    return innerFunction();
+
+}
+outerFunction();
