08fb6898a2e01ddb5b56fa0b25e5534a9b71ac78
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-12 08:51:32 +0000

    New react series with new projects going with the Hooks

---
+++ Basic_React_Series/react-project-three/src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import './Counter.css';
+
+const Counter = () => {
+    return(
+        <div className="counter-container">
+            <p>You have clicked 0 times</p>
+            <button className="btn">Click Me</button>
+        </div>
+    )
+}
+
+export default Counter;
