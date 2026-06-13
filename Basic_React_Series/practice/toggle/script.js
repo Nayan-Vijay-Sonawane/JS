9f58421a593da9b017396c16d9c619675a2a2261
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-13 05:47:55 +0000

    Toggle functionality by only JS

---
+++ Basic_React_Series/practice/toggle/script.js
@@ -0,0 +1,13 @@
+function toggleText(){
+  const text = document.getElementById("text");
+  const button = document.getElementById("toggleBtn");
+
+  if(txet.style.display === "none"){
+    text.style.display = "block";
+    button.textContent = "Hide Text";
+  }
+  else{
+    text.style.display = "none";
+    button.textContent = "Show Text"
+  }
+}
