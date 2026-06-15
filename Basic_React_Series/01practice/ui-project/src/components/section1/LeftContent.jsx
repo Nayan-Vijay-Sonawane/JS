12487127aa7ac70947fe5197b685d59c02b2bbd3
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-15 12:08:27 +0000

    new tailwind ui-project

--- Basic_React_Series/01practice/ui-project/src/components/section1/LeftContent.jsx
+++ Basic_React_Series/01practice/ui-project/src/components/section1/LeftContent.jsx
@@ -2,11 +2,12 @@ import 'remixicon/fonts/remixicon.css'
 
 const LeftContent = () => {
     return (
-        <div className="h-full w-1/4 bg-blue-200 text-6xl font-bold">
-            <div>
-                <h3>Prospective <br/> <span>customer</span> <br/> segmentation</h3>
-            </div>
-            <div className='text-8xl'> 
+        <div className="h-full w-1/4 bg-blue-200">
+            <div className='p-6'>
+            <h3 className='mb-7 text-5xl font-bold'>Prospective <br /><span >customer</span> <br />segmentation</h3>
+            <p className='text-xl w-[90%] font-medium text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias doloremque perspiciatis cumque, eveniet recusandae a laborum vero! Aperiam, at culpa.</p>
+        </div>
+            <div className='text-6xl'> 
                 <i className="ri-arrow-right-up-line"></i>
             </div>
         </div>
