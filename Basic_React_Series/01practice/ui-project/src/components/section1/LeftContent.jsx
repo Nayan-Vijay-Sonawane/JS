06e3bc4bc26882ca9077f308e4375d73beef61d2
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-15 10:32:51 +0000

    new tailwind ui-project

--- Basic_React_Series/01practice/ui-project/src/components/section1/LeftContent.jsx
+++ Basic_React_Series/01practice/ui-project/src/components/section1/LeftContent.jsx
@@ -1,7 +1,12 @@
+import 'remixicon/fonts/remixicon.css'
+
 const LeftContent = () => {
     return (
         <div className="h-full w-1/4 bg-blue-600">
-            LeftContent
+            <div></div>
+            <div className='text-8xl'> 
+                <i className="ri-arrow-right-up-line"></i>
+            </div>
         </div>
     )
 }
--- Basic_React_Series/01practice/ui-project/src/components/section1/RightContent.jsx
+++ Basic_React_Series/01practice/ui-project/src/components/section1/RightContent.jsx
@@ -1,7 +1,7 @@
+
 const RightContent = () => {
     return (
         <div className="h-full w-3/4 bg-blue-600">
-            RightContent
         </div>
     )
 }
