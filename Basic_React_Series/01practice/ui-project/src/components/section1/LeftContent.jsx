de162d095db127ef275c0b48879deead5f8e6ee2
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-15 13:37:37 +0000

    new tailwind ui-project

--- Basic_React_Series/01practice/ui-project/src/components/section1/LeftContent.jsx
+++ Basic_React_Series/01practice/ui-project/src/components/section1/LeftContent.jsx
@@ -4,7 +4,7 @@ const LeftContent = () => {
     return (
         <div className="h-full flex flex-col justify-between w-1/4 ">
             <div className='p-6'>
-            <h3 className='mb-7 leading-[1.3] text-5xl font-bold'>Prospective <br /><span className='bg-gray-200'>customer</span> <br />segmentation</h3>
+            <h3 className='mb-7 leading-[1.3] text-5xl font-bold'>Prospective <br /><span className='bg-gray-200 rounded-2xl'>customer</span> <br />segmentation</h3>
             <p className='text-xl w-[90%] font-medium text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias doloremque perspiciatis cumque, eveniet recusandae a laborum vero! Aperiam, at culpa.</p>
         </div>
             <div className='text-7xl'> 
