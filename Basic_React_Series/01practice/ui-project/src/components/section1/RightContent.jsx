fc9758029f8661e02e6479dc6532f5cac051fabd
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-16 05:51:08 +0000

    new tailwind ui-project

--- Basic_React_Series/01practice/ui-project/src/components/section1/RightContent.jsx
+++ Basic_React_Series/01practice/ui-project/src/components/section1/RightContent.jsx
@@ -2,7 +2,7 @@ import RightCard from "./RightCard";
 
 const RightContent = () => {
     return (
-        <div className="h-full w-2/3 p-6">
+        <div className="h-full flex flex-nowrap w-2/3 p-6">
             <RightCard />
         </div>
     )
