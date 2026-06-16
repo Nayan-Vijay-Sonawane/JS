72ab5d1afc6a0ece89a1c3c88a07a2d4d64d9ec1
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-16 06:31:13 +0000

    new tailwind ui-project

--- Basic_React_Series/01practice/ui-project/src/components/section1/Page1content.jsx
+++ Basic_React_Series/01practice/ui-project/src/components/section1/Page1content.jsx
@@ -1,11 +1,11 @@
 import LeftContent from './LeftContent';
 import RightContent from './RightContent';
 
-const Page1content = () => {
+const Page1content = (props) => {
     return (
         <div className="pb-16 pt-6 flex items-center justify-between gap-10 h-[90vh] px-18">
             <LeftContent />
-            <RightContent />
+            <RightContent users={props.users}/>
         </div>
     )
 }
--- Basic_React_Series/01practice/ui-project/src/components/section1/RightContent.jsx
+++ Basic_React_Series/01practice/ui-project/src/components/section1/RightContent.jsx
@@ -1,11 +1,11 @@
 import RightCard from "./RightCard";
 
-const RightContent = () => {
+const RightContent = (props) => {
     return (
         <div className="h-full flex rounded-4xl overflow-x-auto flex-nowrap gap-10 w-2/3 p-6">
-            <RightCard />
-            <RightCard />
-            <RightCard />
+            {props.users.map(function(elem){
+                return <RightCard img={elem.img}/>
+            })}
         </div>
     )
 }
