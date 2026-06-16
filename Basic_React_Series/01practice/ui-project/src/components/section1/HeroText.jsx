d8a855e7e449bc5b78a4a391990e80875916fe9e
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-16 06:04:51 +0000

    new tailwind ui-project

--- Basic_React_Series/01practice/ui-project/src/components/section1/HeroText.jsx
+++ Basic_React_Series/01practice/ui-project/src/components/section1/HeroText.jsx
@@ -1,7 +1,7 @@
 const HeroText = () => {
     return (
         <div className='p-5'>
-            <h3 className='mb-8 leading-[1.1] text-5xl font-bold'>Prospective <br /><span className='text-gray-500 '>customer</span> <br />segmentation</h3>
+            <h3 className='mb-8 leading-[1.1] text-4xl font-bold'>Prospective <br /><span className='text-gray-500 '>customer</span> <br />segmentation</h3>
             <p className='text-xl w-[90%] font-medium text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias doloremque perspiciatis cumque, eveniet recusandae a laborum vero! Aperiam, at culpa.</p>
         </div>
     )
--- Basic_React_Series/01practice/ui-project/src/components/section1/Section1.jsx
+++ Basic_React_Series/01practice/ui-project/src/components/section1/Section1.jsx
@@ -3,7 +3,7 @@ import Page1content from './Page1content.jsx';
 
 const Section1 = () => {
     return (
-    <div className='h-screen w-full '>
+    <div className='h-screen w-full'>
         <Navbar />
         <Page1content />
     </div>
