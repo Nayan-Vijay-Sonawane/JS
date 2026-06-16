e69b0714f364de9ebd75481bf7d0b2905d0da9a7
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-16 06:38:09 +0000

    new tailwind ui-project

--- Basic_React_Series/01practice/ui-project/src/components/section1/RightContent.jsx
+++ Basic_React_Series/01practice/ui-project/src/components/section1/RightContent.jsx
@@ -3,8 +3,8 @@ import RightCard from "./RightCard";
 const RightContent = (props) => {
     return (
         <div className="h-full flex rounded-4xl overflow-x-auto flex-nowrap gap-10 w-2/3 p-6">
-            {props.users.map(function(elem){
-                return <RightCard img={elem.img} tag={elem.tag}/>
+            {props.users.map(function(elem, idx){
+                return <RightCard key={idx} img={elem.img} tag={elem.tag}/>
             })}
         </div>
     )
