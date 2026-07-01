b1e62c7ca198e1ec60d392de552c111bd1172e7f
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-01 11:53:18 +0000

    New projects going with the form handling

--- Basic_React_Series/08-form-handling/src/App.jsx
+++ Basic_React_Series/08-form-handling/src/App.jsx
@@ -2,11 +2,15 @@ import './App.css'
 
 function App() {
 
+  const submitHandler = () =>{
+    console.log('Form submitted succesful!');
+  }
+
   return (
     <div className='h-screen w-full bg-blue-950 flex flex-col justify-center items-center gap-10'>
-      <form className='flex flex-col gap-10'>
-        <input type="text" placeholder='Enter your name' className='bg-gray-400 px-8 py-2.5' />
-        <button className='bg-gray-500 px-2 py-1.5 rounded-md text-white cursor-pointer'>submit</button>
+      <form onSubmit={submitHandler} className='flex flex-col gap-10 items-start'>
+        <input type="text" placeholder='Enter your name' className='bg-gray-400 w-full px-6 py-2.5 rounded-sm' />
+        <button className='bg-gray-500 px-2 py-1.5 w-fit rounded-md text-white cursor-pointer'>submit</button>
       </form>
 
     </div>
