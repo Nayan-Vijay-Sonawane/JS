c0da8e72174fffac6eac6fec3d01d1d1fc1c8c43
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-01 12:10:34 +0000

    New projects going with the form handling with two way binding

--- Basic_React_Series/08-form-handling/src/App.jsx
+++ Basic_React_Series/08-form-handling/src/App.jsx
@@ -1,17 +1,25 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
+  const [title, setTitle] = useState('');
+
   const submitHandler = (e) =>{
     // preveting default behaviour of form (reloading)
     e.preventDefault();
-    console.log('Form submitted succesful!');
+    console.log('Form submitted by', title);
   }
 
   return (
     <div className='h-screen w-full bg-blue-950 flex flex-col justify-center items-center gap-10'>
       <form onSubmit={(e) => {submitHandler(e)}} className='flex flex-col gap-10 items-start'>
-        <input type="text" placeholder='Enter your name' className='bg-gray-400 w-full px-6 py-2.5 rounded-sm' />
+        <input 
+        type="text" 
+        placeholder='Enter your name'
+        value={title} 
+        onChange={(e) => setTitle(e.target.value)}
+        className='bg-gray-400 w-full px-6 py-2.5 rounded-sm' />
         <button className='bg-gray-500 px-2 py-1.5 w-fit rounded-md text-white cursor-pointer'>submit</button>
       </form>
 
