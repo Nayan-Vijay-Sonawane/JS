add5fd82a9abcd4cc74695e8d2bb19e54edbb504
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-01 11:37:55 +0000

    New projects going with the form handling

--- Basic_React_Series/08-form-handling/src/App.jsx
+++ Basic_React_Series/08-form-handling/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='h-screen w-full bg-blue-950 flex flex-col justify-center items-center gap-10'>
+      <form className='flex flex-col gap-10'>
+        <input type="text" placeholder='Enter your name' className='bg-gray-400 px-8 py-2.5' />
+        <button className='bg-gray-500 px-2 py-1.5 rounded-md text-white cursor-pointer'>submit</button>
+      </form>
 
     </div>
   )
