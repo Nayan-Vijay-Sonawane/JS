e8cf2995c1b1821b7ffe52de8ab4fdf98898f0eb
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-01 12:19:02 +0000

    New projects going with the form handling with two way binding

--- Basic_React_Series/08-form-handling/src/App.jsx
+++ Basic_React_Series/08-form-handling/src/App.jsx
@@ -17,8 +17,9 @@ function App() {
         <input 
         type="text" 
         placeholder='Enter your name'
-        // 
+        // Bind the input's value to state
         value={title} 
+        // Updates the state whenever the user types
         onChange={(e) => setTitle(e.target.value)}
         className='bg-gray-400 w-full px-6 py-2.5 rounded-sm' />
         <button className='bg-gray-500 px-2 py-1.5 w-fit rounded-md text-white cursor-pointer'>submit</button>
