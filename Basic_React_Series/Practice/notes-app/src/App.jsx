3b4dbe357b591bf7596e5b7d5dd480d9025ed2f1
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-02 09:58:55 +0000

    new project notes app

--- Basic_React_Series/Practice/notes-app/src/App.jsx
+++ Basic_React_Series/Practice/notes-app/src/App.jsx
@@ -2,16 +2,23 @@ import './App.css'
 
 function App() {
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  }
+
   return (
-    <div className='h-screen w-full bg-black text-white'>
-      <form className='flex justify-between items-start gap-10 p-10'>
-        <div className='flex items-start flex-col'>
-          <input type="text" placeholder='Enter notes heading' className='px-5 w-1/2 py-2 border-2 rounded' />
-          <input type="text" placeholder='Write details' className='px-5 w-1/2 h-18 py-2 border-2 rounded' />
-          <button className='bg-white text-black px-5 w-1/2 py-2 cursor-pointer'>Add Note</button>
-        </div>
-        <img className='h-80' src="https://static.vecteezy.com/system/resources/thumbnails/051/865/736/small_2x/yellow-post-it-note-with-thumbtack-isolated-on-transparent-background-png.png" alt="" />
+    <div className='h-screen w-full bg-black text-white p-10'>
+      <form onSubmit={(e) => submitHandler(e)} className='flex flex-col justify-between items-start gap-4 w-1/2 p-10'>
+        <input type="text" placeholder='Enter notes heading' className='px-5 w-full py-2 font-medium border-2 outline-none rounded' />
+        <textarea type="text" placeholder='Write details' className='px-5 w-full h-32 font-medium py-2 border-2 outline-none rounded' />
+        <button className='bg-white text-black px-5 w-full py-2 outline-none font-medium rounded cursor-pointer'>Add Note</button>
       </form>
+      <div className='flex flex-wrap p-10'>
+        <div className='h-32 w-32 rounded-2xl bg-white'>
+        </div>
+      </div>
+
     </div>
   )
 }
