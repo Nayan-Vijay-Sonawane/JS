24623584eb03d03205cd08e3519b1ce88099bbe7
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-05 18:32:29 +0000

    New react series with new projects going with the event handling

--- Basic_React_Series/10-event-handling/src/App.jsx
+++ Basic_React_Series/10-event-handling/src/App.jsx
@@ -11,9 +11,18 @@ function App() {
     alert("Mouse over on para")
   }
 
+  const handleInputChange = () => {
+
+  }
+
 
   return (
     <div className='bg-blue-950 h-screen flex flex-col gap-5 justify-center items-center text-white'>
+
+    <form>
+      <input type="text" onChange={handleInputChange} />
+    </form>
+
       <p onMouseOver={handleMouseover} className='border p-3'>I am a paragraph</p>
       <button onClick={handleClick} className='bg-gray-500 px-5 py-2 rounded active:scale-95 cursor-pointer'>Click Me</button>
     </div>
