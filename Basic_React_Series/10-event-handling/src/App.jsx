ff5639998f41e17fabcac1732b91568fcecb550f
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-05 18:17:07 +0000

    New react series with new projects going with the event handling

--- Basic_React_Series/10-event-handling/src/App.jsx
+++ Basic_React_Series/10-event-handling/src/App.jsx
@@ -3,10 +3,14 @@ import './App.css'
 
 function App() {
 
+  const handleClick = () => {
+    alert("I am clicked")
+  }
+
 
   return (
     <div className='bg-blue-950 h-screen flex justify-center items-center text-white'>
-      <button className='bg-gray-500 px-5 py-2 rounded active:scale-95 cursor-pointer'>Click Me</button>
+      <button onClick={handleClick} className='bg-gray-500 px-5 py-2 rounded active:scale-95 cursor-pointer'>Click Me</button>
     </div>
   )
 }
