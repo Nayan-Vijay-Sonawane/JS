ada2cead7342049b7ac4c1257ea2874b578d9d6e
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-06 18:52:12 +0000

    New react series with new projects going with the local storage

--- Basic_React_Series/11-storage/src/App.jsx
+++ Basic_React_Series/11-storage/src/App.jsx
@@ -2,14 +2,32 @@ import './App.css'
 
 function App() {
 
+  // to store the value
+  localStorage.setItem("user", "Nayan");
+
+  // to get the value (display)
+  const user = localStorage.getItem("user");
+  console.log(user);
+
+  // to remove the value
+  localStorage.removeItem("user");
+
+  // storing object
+  const obj1 = {
+    name: "Nayan",
+    age: 25,
+    isStudent: true
+  }
+
+  // since the localstorage only stores strings to need to convert this into string
+  localStorage.setItem("obj1", JSON.stringify(obj1));
+
+  const userString = JSON.parse(localStorage.getItem("obj1"));
+  console.log(userString);
+
   return (
-    <div className='h-screen bg-black text-white p-10 '>
-      <ul className='flex justify-evenly'>
-        <li><a href='#'>Home</a></li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Login</li>
-      </ul>
+    <div className='h-screen flex justify-center items-center bg-black text-white p-10 '>
+      Local Storage
     </div>
   )
 }
