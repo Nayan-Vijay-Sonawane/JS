82c2d84696ea0fc7755cba21ab687e4d2b5912ad
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-06 11:37:50 +0000

    new project random color generator

--- Basic_React_Series/Practice/random-color/src/App.jsx
+++ Basic_React_Series/Practice/random-color/src/App.jsx
@@ -5,6 +5,10 @@ import './App.css';
 function App() {
   const [clicks, setClicks] = useState(0);
 
+  const handleClickOutside = () => {
+    setClicks(c => c + 1);
+  }
+
   const getRandomColor = () => {
     let r = 150 + Math.round(100 * Math.random());
     let g = 150 + Math.round(100 * Math.random());
@@ -19,10 +23,13 @@ function App() {
   }
 
   return (
-    <div className='bg-gray-700 h-screen flex justify-center items-center text-white text-2xl'>
+    <div onClick={handleClickOutside} className='bg-gray-500 h-[100%] w-[100%] flex flex-col justify-center items-center text-white text-2xl m-5 p-5'>
       <Colorswitch onChangeColor={handleChangeColor}/>
+      <br />
+      <br />
+      <h2 className=''>Clicks on the page: {clicks}</h2>
     </div>
   )
 }
 
-export default App
+export default App;
