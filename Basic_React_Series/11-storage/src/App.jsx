4969506079e9fe52006fb2496f0a9cf0b087e663
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-06 16:20:39 +0000

    New react series with new projects going with the local storage

--- Basic_React_Series/11-storage/src/App.jsx
+++ Basic_React_Series/11-storage/src/App.jsx
@@ -3,10 +3,15 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      App
+    <div className='h-screen bg-black text-white p-10 '>
+      <ul className='flex justify-evenly'>
+        <li><a href='#'>Home</a></li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Login</li>
+      </ul>
     </div>
   )
 }
 
-export default App
+export default App;
