b810b9463f81f3fb11ca8f513a702e8d47729d78
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-07 14:22:05 +0000

    New react series with new projects going with API calling

--- Basic_React_Series/12-api-calling/src/App.jsx
+++ Basic_React_Series/12-api-calling/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+
+    const data = await res.json()
+    console.log(data);
+  }
 
   return (
-    <div>
-      App
+    <div className='h-screen bg-gray-950 text-white flex justify-center items-center'>
+      <button onClick={getData} className='px-5 py-2 bg-gray-600 cursor-pointer active:scale-95 rounded'>Get Data</button>
     </div>
   )
 }
