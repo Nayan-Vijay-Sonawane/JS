ae64f9cfcc5793b921b35fd4d00b92dfdcb5db6d
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-08 14:08:49 +0000

    New react series with new projects going with useEffect hook

--- Basic_React_Series/13-useEffect/src/App.jsx
+++ Basic_React_Series/13-useEffect/src/App.jsx
@@ -4,9 +4,14 @@ import './App.css'
 function App() {
 
   const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(1);
 
   const handleCount = () => {
     setCount(count+1);
+  }
+
+  const handleCountTotal = () => {
+    setTotal(total+1);
   }
 
   // Variation-1 (runs on every render)
@@ -15,18 +20,28 @@ function App() {
   // });
 
   // Variation-2 (runs only once(on Mount))
-  useEffect(() => {
-    alert("Runs only once")
-  }, []);
+  // useEffect(() => {
+  //   alert("Runs only once")
+  // }, []);
 
   // Vairation-3 (runs when a value changes)
+  useEffect(() => {
+    alert("Runs everytime when the count is updated")
+  }, [count]);
 
+  // Variation-4 (multiple dependencies)
+  useEffect(() => {
+
+  }, [])
 
 
   return (
     <div className='p-5 h-screen flex flex-col justify-center items-center gap-5'>
       <h2>Count is : {count}</h2>
-      <button onClick={handleCount} className='bg-black rounded px-5 py-2 text-white cursor-pointer active:scale-95'>Click Me</button>
+      <button onClick={handleCount} className='bg-black rounded px-5 py-2 text-white cursor-pointer active:scale-95'>Update Count</button>
+      
+      <h2>Total is : {total}</h2>
+      <button onClick={handleCountTotal} className='bg-black rounded px-5 py-2 text-white cursor-pointer active:scale-95'>Update Total</button>
     </div>
   )
 }
