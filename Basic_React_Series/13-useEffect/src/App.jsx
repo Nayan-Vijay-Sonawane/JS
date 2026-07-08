135fb811709454e553d7d58e8e57b278ce33b1b1
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-08 10:30:15 +0000

    New react series with new projects going with useEffect hook

--- Basic_React_Series/13-useEffect/src/App.jsx
+++ Basic_React_Series/13-useEffect/src/App.jsx
@@ -1,15 +1,26 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => {
+    setCount(count+1);
+  }
+
+  // Variation-1 (runs on every render)
   useEffect(() => {
-    
-  }, []);
+    alert("Runs on every render");
+  });
+
+  // Variation-2 (runs only once(on Mount))
+
 
   return (
     <div>
-      App
+      <h2>Count is : {count}</h2>
+      <button onClick={handleCount} className='bg-black rounded px-5 py-2 text-white cursor-pointer active:scale-95'>Click Me</button>
     </div>
   )
 }
