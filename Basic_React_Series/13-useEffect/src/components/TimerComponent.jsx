9acaa02bfa5afccda59bd0b187ff98931b0b0d2d
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-09 09:03:28 +0000

    New react series with new projects going with useEffect hook

--- Basic_React_Series/13-useEffect/src/components/TimerComponent.jsx
+++ Basic_React_Series/13-useEffect/src/components/TimerComponent.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 const TimerComponent = () => {
     const [seconds, setSeconds] = useState(0);
 
-    // Runs only on first render
+    // Runs only on first render 
     useEffect(() => {
         const intervalId = setInterval(() => {
             setSeconds(prevSeconds => prevSeconds + 1);
