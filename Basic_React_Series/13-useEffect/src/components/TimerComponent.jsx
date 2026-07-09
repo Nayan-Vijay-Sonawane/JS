d27dca12ed310de1fbaec8afba31be30a09dd655
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-09 09:04:07 +0000

    New react series with new projects going with useEffect hook

--- Basic_React_Series/13-useEffect/src/components/TimerComponent.jsx
+++ Basic_React_Series/13-useEffect/src/components/TimerComponent.jsx
@@ -13,7 +13,7 @@ const TimerComponent = () => {
             clearInterval(intervalId);
         }
     }, []);  
- 
+  
     return(
         <div className="h-screen flex flex-col gap-5 justify-center items-center">
             <h1 className="font-bold text-4xl">Seconds : {seconds}</h1>
