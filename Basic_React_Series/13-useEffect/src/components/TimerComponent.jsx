c73b11b3a3c7af1d6ff25484ae56edea123b881d
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-10 11:24:01 +0000

    New react series with new projects going with useEffect hook

--- Basic_React_Series/13-useEffect/src/components/TimerComponent.jsx
+++ Basic_React_Series/13-useEffect/src/components/TimerComponent.jsx
@@ -6,8 +6,14 @@ const TimerComponent = () => {
     // Runs only on first render 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setSeconds(prevSeconds => prevSeconds + 1);
             // setSeconds(seconds+1);
+            // seconds=0 -> The interval callback remembers this value because of js closure
+            // seconds=0 -> setSeconds(0+1) -> Since the effects runs only once, the callback keeps using the initial value
+            // it never increase beyond 1 
+
+            setSeconds(prevSeconds => prevSeconds + 1);
+            // this is called functional updater
+            
         }, 1000);
 
         return () => {
