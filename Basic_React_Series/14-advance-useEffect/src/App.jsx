69ad02894b6573aa50086a61d785f68d87e6ccf2
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-11 14:30:25 +0000

    New react series with new projects going with  advance useEffect hook

--- Basic_React_Series/14-advance-useEffect/src/App.jsx
+++ Basic_React_Series/14-advance-useEffect/src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
-import FetchMovies from './components/FetchMovies'
+import { useState, useEffect } from 'react';
 
 function App() {
 
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  useEffect(() => {
+    console.log("Value of A is changing...");
+  }, [a])
+
+  useEffect(() => {
+    console.log("Value of B is changing...");
+  }, [b])
+
+
   return (
-    <div className='h-screen bg-black text-white flex justify-center items-center'>
-      <FetchMovies />
+    <div className='h-screen bg-black text-white flex flex-col gap-5 justify-center items-center'>
+      <div></div>
+      <h1 className='text-3xl text-emerald-50'>A is {a}</h1>
+      <h2 className='text-3xl text-emerald-50'>B is {b}</h2>
+      <button onClick={() => {setA(a+1)}} className='px-5 py-2 rounded cursor-pointer active:scale-95 bg-gray-500 text-white'>Change A</button>
+      <button onClick={() => {setB(b-1)}} className='px-5 py-2 rounded cursor-pointer active:scale-95 bg-gray-500 text-white'>Change B</button>
     </div>
   )
 }
