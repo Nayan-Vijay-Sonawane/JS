501e0a6a61b403839a05b73c67e606e1787fbc1e
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-11 06:50:58 +0000

    New react series with new projects going with useEffect hook

--- Basic_React_Series/13-useEffect/src/components/MultiEffectComponent.jsx
+++ Basic_React_Series/13-useEffect/src/components/MultiEffectComponent.jsx
@@ -5,11 +5,26 @@ const MultiEffectComponent = () => {
     const [count, setCount] = useState(0);
     const [seconds, setSeconds] = useState(0);
 
+    // side effect logic will run only when the count is changed 
+    useEffect(() => {
+        console.log("Count changed: ", count);
+    }, [count]);
+
+    // Runs only on first render
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setSeconds(prevSeconds => prevSeconds + 1);
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
 
         <div className="h-screen gap-5 bg-black text-white flex flex-col justify-center items-center">
-            <h1 className="text-3xl ">Count: {count}</h1>
+            <h1 className="text-3xl font-bold">Count: {count}</h1>
             <button className="bg-gray-500 px-5 py-2 rounded cursor-pointer active:scale-95" onClick={() => setCount(count+1)}>Increment Count</button>
+            <h2 className="text-2xl font-bold">Seconds: {seconds}</h2>
         </div>
 
     )
