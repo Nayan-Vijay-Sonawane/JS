8b5c3d78818358dfcd90e79e9a22fa9aaa9860e9
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-11 14:34:57 +0000

    New react series with new projects going with advance useEffect hook

--- Basic_React_Series/14-advance-useEffect/src/App.jsx
+++ Basic_React_Series/14-advance-useEffect/src/App.jsx
@@ -17,11 +17,11 @@ function App() {
 
   return (
     <div className='h-screen bg-black text-white flex flex-col gap-5 justify-center items-center'>
-      <div className='flex gap-10'>
+      <div className='flex gap-15'>
         <h1 className='text-3xl text-emerald-50'>A is {a}</h1>
         <h2 className='text-3xl text-emerald-50'>B is {b}</h2>
       </div>
-      <div>
+      <div className='flex gap-10'>
         <button onClick={() => { setA(a + 1) }} className='px-5 py-2 rounded cursor-pointer active:scale-95 bg-gray-500 text-white'>Change A</button>
         <button onClick={() => { setB(b - 1) }} className='px-5 py-2 rounded cursor-pointer active:scale-95 bg-gray-500 text-white'>Change B</button>
       </div>
