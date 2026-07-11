c8980cb275c06cfef24cf3f30e41812e262392a0
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-11 06:46:04 +0000

    New react series with new projects going with useEffect hook

--- Basic_React_Series/13-useEffect/src/components/MultiEffectComponent.jsx
+++ Basic_React_Series/13-useEffect/src/components/MultiEffectComponent.jsx
@@ -7,7 +7,10 @@ const MultiEffectComponent = () => {
 
     return (
 
-        <div></div>
+        <div className="h-screen gap-5 bg-black text-white flex flex-col justify-center items-center">
+            <h1 className="text-3xl ">Count: {count}</h1>
+            <button className="bg-gray-500 px-5 py-2 rounded cursor-pointer active:scale-95" onClick={() => setCount(count+1)}>Increment Count</button>
+        </div>
 
     )
 }
