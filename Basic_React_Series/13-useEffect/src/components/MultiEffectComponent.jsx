cfbc059578fb1f2094682075654d9f9d5dbf1f22
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-11 06:42:33 +0000

    New react series with new projects going with useEffect hook

---
+++ Basic_React_Series/13-useEffect/src/components/MultiEffectComponent.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+const MultiEffectComponent = () => {
+
+    const [count, setCount] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+    return (
+
+        <div></div>
+
+    )
+}
+
+export default MultiEffectComponent;
