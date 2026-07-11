b381887840d92f07fff91a4c11d78b3b2b077018
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-11 14:39:26 +0000

    New react series with new projects going with advance useEffect hook

--- Basic_React_Series/14-advance-useEffect/src/App.jsx
+++ Basic_React_Series/14-advance-useEffect/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         <button onClick={() => { setA(a + 1) }} className='px-5 py-2 rounded cursor-pointer active:scale-95 bg-gray-500 text-white'>Change A</button>
         <button onClick={() => { setB(b - 1) }} className='px-5 py-2 rounded cursor-pointer active:scale-95 bg-gray-500 text-white'>Change B</button>
       </div>
-
+    
     </div> 
   )
 }
