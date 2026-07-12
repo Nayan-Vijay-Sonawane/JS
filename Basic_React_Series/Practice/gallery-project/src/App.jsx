b5a04734ddda947ee0cba693d91c67802c1fa1bf
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-12 13:28:28 +0000

    new project gallery project using useEffect

--- Basic_React_Series/Practice/gallery-project/src/App.jsx
+++ Basic_React_Series/Practice/gallery-project/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className='h-screen flex justify-center items-center'>
+    <div className='h-screen bg-black flex justify-center items-center'>
       App
     </div>
   )
