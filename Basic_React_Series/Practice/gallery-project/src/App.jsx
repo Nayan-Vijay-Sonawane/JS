d7e3ee52200036be4ee4a2df57fc0f50fad9bac0
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-13 15:38:30 +0000

    new project gallery project using useEffect

--- Basic_React_Series/Practice/gallery-project/src/App.jsx
+++ Basic_React_Series/Practice/gallery-project/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 function App() {
 
   const [userData, setUserData] = useState([]);
+  const [index, setIndex] = useState(1);
 
   const getData = async () => {
     const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=30');
