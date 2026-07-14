f02938c074169a88be490395aa37b95597eba2b6
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-14 16:06:40 +0000

    New react series with new projects going with useContext hook

--- Basic_React_Series/15-useContext/src/App.jsx
+++ Basic_React_Series/15-useContext/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className='bg-black h-screen text-white'>
       {/* Step-2 : Wrap the childs inside a provider */} 
+      {/* Step-3 : pass the value */}
       <UserContext.Provider value="Nayan">
         <Parent />
       </UserContext.Provider>
