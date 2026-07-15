a07e9198b38ed741b62d83a9f9d2d5d3bea6e7a9
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-15 17:13:58 +0000

    New react series with new projects going with useContext hook

--- Basic_React_Series/15-useContext/src/App.jsx
+++ Basic_React_Series/15-useContext/src/App.jsx
@@ -4,7 +4,7 @@ import Parent from './components/Parent';
 import UserContext from './context/UserContext';
 function App() { 
   const [user, setUser] = useState({name: "Nayan"}); 
-
+ 
   return ( 
     <div className='bg-black h-screen text-white p-5'>
       {/* Step-2 : Wrap the childs inside a provider */} 
