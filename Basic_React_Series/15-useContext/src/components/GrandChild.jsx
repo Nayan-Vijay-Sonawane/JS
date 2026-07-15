48ea908e4e230b0c719b074b45eefa8b57510e1a
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-15 17:09:05 +0000

    New react series with new projects going with useContext hook

--- Basic_React_Series/15-useContext/src/components/GrandChild.jsx
+++ Basic_React_Series/15-useContext/src/components/GrandChild.jsx
@@ -4,7 +4,7 @@ import UserContext from "../context/UserContext";
 const GrandChild = () => {
 
     const user = useContext(UserContext);
-
+    // Step-4 : Consume the data from the consumer
     return (
         <div>
             <h1>Hello this is {user.name}</h1>
