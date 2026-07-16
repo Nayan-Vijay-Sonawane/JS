3dd0681864e2a1706149636972749a83c46fd8b0
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-16 06:43:24 +0000

    New theme project using useContext

--- Basic_React_Series/Practice/theme-project/src/App.jsx
+++ Basic_React_Series/Practice/theme-project/src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
 
   return (
-    <div className='h-screen bg-mauve-600 text-white'>
-      App
+    <div className='h-screen bg-mauve-500 text-white p-10'>
+      <button className='bg-mauve-800 px-5 py-2 rounded cursor-pointer active:scale-95'>Click Me</button>
     </div>
   )
 }
