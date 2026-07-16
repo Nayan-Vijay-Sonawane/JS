a84f37fb9350f70ea245731fdfae0870cabcd92c
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-16 09:01:06 +0000

    New theme project using useContext

--- Basic_React_Series/Practice/theme-project/src/components/GrandChild.jsx
+++ Basic_React_Series/Practice/theme-project/src/components/GrandChild.jsx
@@ -6,8 +6,8 @@ const GrandChild = () => {
     const theme = useContext(ThemeContext);
 
     return (
-        <div className="p-10">
-            {theme}
+        <div className='h-screen bg-mauve-500 text-white p-10'>
+            <button className='bg-mauve-800 px-5 py-2 rounded cursor-pointer active:scale-95'>Click Me</button>
         </div>
     )
 }
