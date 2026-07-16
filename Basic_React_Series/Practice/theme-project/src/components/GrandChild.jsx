837f7f7896cf123f3c5d4326a2a6fef085470bae
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-16 09:17:29 +0000

    New theme project using useContext

--- Basic_React_Series/Practice/theme-project/src/components/GrandChild.jsx
+++ Basic_React_Series/Practice/theme-project/src/components/GrandChild.jsx
@@ -3,12 +3,21 @@ import ThemeContext from '../context/ThemeContext';
 
 const GrandChild = () => {
 
-    const theme = useContext(ThemeContext);
+    const { theme, setTheme } = useContext(ThemeContext);
+
+    const handleClick = () => {
+        if(theme === 'light'){
+            setTheme('dark')
+        }
+        else{
+            setTheme('light')
+        }
+    }
 
     return (
         <div className='h-screen bg-mauve-500 text-white p-10'>
             <div className="h-100 w-100 border-2 border-black flex flex-col justify-center items-center">
-                <button className='bg-mauve-800 px-5 py-2 rounded cursor-pointer active:scale-95'>Change Theme</button>
+                <button onClick={handleClick} className='bg-mauve-800 px-5 py-2 rounded cursor-pointer active:scale-95'>Change Theme</button>
             </div>
         </div>
     )
