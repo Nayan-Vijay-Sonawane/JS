e606790b78f87bc28ade4e5965ec1cd21ae30fa8
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-18 16:17:32 +0000

    New react series with new projects going with routing with react-router-dom

--- Basic_React_Series/16-react-router-project/src/App.jsx
+++ Basic_React_Series/16-react-router-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css'
+import './App.css';
 import Home from './components/Home';
 import About from './components/About';
 import Dashboard from './components/Dashboard';
