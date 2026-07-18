05804cf4100a05ef6a632ef84fd7cbf252921df5
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-18 16:15:56 +0000

    New react series with new projects going with routing with react-router-dom

--- Basic_React_Series/16-react-router-project/src/components/About.jsx
+++ Basic_React_Series/16-react-router-project/src/components/About.jsx
@@ -2,7 +2,7 @@
 const About = () => {
     return (
         <div className="text-2xl font-bold text-amber-50 p-10 flex justify-center items-center">
-            About
+            About 
         </div>
     )
 
