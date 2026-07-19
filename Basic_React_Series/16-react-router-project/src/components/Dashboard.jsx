5dd705cdb2adf1affa70cbd289bec57d3bb56543
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-19 12:18:56 +0000

    New react series with new projects going with routing with react-router-dom

--- Basic_React_Series/16-react-router-project/src/components/Dashboard.jsx
+++ Basic_React_Series/16-react-router-project/src/components/Dashboard.jsx
@@ -2,7 +2,7 @@
 const Dashboard = () => {
     return (
         <div className="text-2xl font-bold text-mist-300 p-10 flex justify-center items-center">
-            Dashboard 
+            Dashboard Page
         </div>
     )
 }; 
