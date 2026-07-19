eb688fadbd40a2f006274ca64adbf5d6a7097e3e
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-19 12:19:07 +0000

    New react series with new projects going with routing with react-router-dom

--- Basic_React_Series/16-react-router-project/src/components/Home.jsx
+++ Basic_React_Series/16-react-router-project/src/components/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div className="text-2xl font-bold p-10 flex justify-center items-center">
-            Home 
+            Home Page
         </div>
     )
 }
