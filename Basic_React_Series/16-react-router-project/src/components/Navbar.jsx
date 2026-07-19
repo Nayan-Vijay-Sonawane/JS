b5bf5b09800cbde7ab87a8dc1ceb28169efc3ac4
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-19 12:39:43 +0000

    New react series with new projects going with routing with react-router-dom

--- Basic_React_Series/16-react-router-project/src/components/Navbar.jsx
+++ Basic_React_Series/16-react-router-project/src/components/Navbar.jsx
@@ -4,15 +4,15 @@ const Navbar = () => {
     return (
         <div>
             <ul className="flex justify-evenly">
-                <li><NavLink to='/' > 
+                <li><NavLink to='/' className={({isActive}) => isActive ? 'active-link' : ''}> 
                 Home
                 </NavLink>
                 </li>
-                <li><NavLink to='/about' > 
+                <li><NavLink to='/about' className={({isActive}) => isActive ? 'active-link' : ''}> 
                 About
                 </NavLink>
                 </li>
-                <li><NavLink to='/dashboard' > 
+                <li><NavLink to='/dashboard' className={({isActive}) => isActive ? 'active-link' : ''}> 
                 Dashboard
                 </NavLink>
                 </li>
