45bd351ff1818d04cc9f42922d460ed22c8e1413
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-21 16:30:01 +0530

    New react series with new projects going with routing with react-router-dom

--- Basic_React_Series/16-react-router-project/src/components/Dashboard.jsx
+++ Basic_React_Series/16-react-router-project/src/components/Dashboard.jsx
@@ -1,8 +1,10 @@
+import { Outlet } from 'react-router-dom'; 
 
 const Dashboard = () => {
     return (
         <div className="text-2xl font-bold text-mist-300 p-10 flex justify-center items-center">
             Dashboard Page
+            <Outlet />
         </div>
     )
 }; 
