d57e758b217a0409148d8a2d9c81b26ab56e504a
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-21 13:28:20 +0530

    New react series with new projects going with routing with react-router-dom

--- Basic_React_Series/16-react-router-project/src/components/About.jsx
+++ Basic_React_Series/16-react-router-project/src/components/About.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 const About = () => {
     const navigate = useNavigate();
     const handleClick = () => {
-        
+        navigate('/dashboard');
     }
     
     return (
