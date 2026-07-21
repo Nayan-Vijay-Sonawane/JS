279d587787a51f5c7abc7bd2a88e98606adcd902
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-21 13:27:25 +0530

    New react series with new projects going with routing with react-router-dom

--- Basic_React_Series/16-react-router-project/src/components/About.jsx
+++ Basic_React_Series/16-react-router-project/src/components/About.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 const About = () => {
     const navigate = useNavigate();
+    const handleClick = () => {
+        
+    }
     
     return (
         <div className="text-2xl font-bold text-amber-50 p-10 flex justify-center items-center">
