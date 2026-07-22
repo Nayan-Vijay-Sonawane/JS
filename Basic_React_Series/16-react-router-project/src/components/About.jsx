2e80e2b6f8c825051d833017cb1cb7475c16f235
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-07-22 23:41:12 +0530

    New react series with new projects going with routing with react-router-dom

--- Basic_React_Series/16-react-router-project/src/components/About.jsx
+++ Basic_React_Series/16-react-router-project/src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         <div className="text-2xl font-bold text-amber-50 p-10 flex justify-center items-center">
             About Page
         </div>
-        <button
+        <button 
         onClick={handleClick}
         className='bg-amber-50 text-black rounded px-5 py-2 cursor-pointer active:scale-95'>
             Move to Dashboard
