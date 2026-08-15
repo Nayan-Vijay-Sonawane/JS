85f1ef072d15b37a8c63e26e335d74a1b4ec2940
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-08-15 23:27:54 +0530

    New react series with new projects going with routing with advance react-router-dom

--- Basic_React_Series/17-react-router-advance/src/App.jsx
+++ Basic_React_Series/17-react-router-advance/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='h-screen bg-black text-white'>
       <h1 className='text-2xl font-bold flex justify-center items-center'>App</h1> 
-      <Card />
+      <Card /> 
     </div> 
   )  
 };
