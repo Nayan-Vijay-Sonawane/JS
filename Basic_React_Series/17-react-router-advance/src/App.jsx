2dd858be28dd74e2e1a994421435950b344656db
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-08-18 21:43:08 +0530

    New react series with new projects going with routing with advance react-router-dom

--- Basic_React_Series/17-react-router-advance/src/App.jsx
+++ Basic_React_Series/17-react-router-advance/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './pages/Home'
 import About from './pages/About'
-import Contact from './pages/Contact';
+import Contact from './pages/Contact'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom';
 import Product from './pages/Product';
