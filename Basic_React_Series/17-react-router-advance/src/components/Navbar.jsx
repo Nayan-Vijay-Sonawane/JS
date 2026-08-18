4a15eea4e3700cd2237a04e7ad93b69430be51b3
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-08-18 21:58:39 +0530

    New react series with new projects going with routing with advance react-router-dom

--- Basic_React_Series/17-react-router-advance/src/components/Navbar.jsx
+++ Basic_React_Series/17-react-router-advance/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => { 
  return ( 
@@ -12,7 +12,7 @@ const Navbar = () => {
                 </div>
             </div> 
  )  
-}
+};
 
 export default Navbar;
 
