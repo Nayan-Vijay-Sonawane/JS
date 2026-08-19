796830a4b4e7f7bcaa12ebd658c52a0f44302ff0
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-08-19 18:56:14 +0530

    New react series with new projects going with routing with advance react-router-dom

--- Basic_React_Series/17-react-router-advance/src/pages/Product.jsx
+++ Basic_React_Series/17-react-router-advance/src/pages/Product.jsx
@@ -1,9 +1,9 @@
-const Product = () => {
+;const Product = () => {
     return (
         <div className="h-screen flex justify-center items-center">
             <h1 className="text-3xl font-bold underline">Products</h1>
         </div>
     )
-}
+};
 
 export default Product;
