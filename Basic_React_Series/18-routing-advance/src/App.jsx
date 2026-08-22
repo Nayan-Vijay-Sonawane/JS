75ed76ea59bc866a3211466c84f0b9d80158593f
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-08-22 22:14:35 +0530

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/App.jsx
+++ Basic_React_Series/18-routing-advance/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className='h-screen bg-amber-900 flex justify-center'>
+    <div className='h-screen bg-amber-900 flex justify-center items'>
       <h1 className='text-2xl font-bold'>App</h1>
     </div>
   )
