65f788eff317afd8b8ee3dd7cfd1e4b9635663a2
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-08-26 13:21:03 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/components/Navbar.jsx
+++ Basic_React_Series/18-routing-advance/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
     return (
-        <div className="flex justify-between">
+        <div className="py-4 px-8 flex justify-between">
             <h2>Furn-Tech</h2>
-            <div>
+            <div className="flex gap-8">
                 <a href="/">Home</a>
                 <a href="/about">About</a>
                 <a href="/contact">Contact</a>
