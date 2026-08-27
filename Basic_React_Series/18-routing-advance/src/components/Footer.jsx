cbf00379f8860730504209f5681eb668dedcc935
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-08-27 18:00:01 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/components/Footer.jsx
+++ Basic_React_Series/18-routing-advance/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="absolute flex items-center justify-center">
+        <div className="absolute w-full  flex items-center justify-center">
             <h3>This is Footer</h3>
         </div>
     )
