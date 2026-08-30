8126d516601a120a37c5737044c1cabf3c20506c
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-08-30 12:12:58 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/App.jsx
+++ Basic_React_Series/18-routing-advance/src/App.jsx
@@ -3,6 +3,8 @@ import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Product from './pages/Product'
 
 function App() {
  
@@ -10,8 +12,9 @@ function App() {
     <div className='h-screen bg-black text-white'>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/product' element={<Product />} />
       </Routes>
       <Footer />
     </div>
