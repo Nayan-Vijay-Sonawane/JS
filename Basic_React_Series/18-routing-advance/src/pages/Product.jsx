cbe99ecd042f4a4abf3ad5b0c28bcc230b8a3288
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-08-31 21:56:42 +0530

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/pages/Product.jsx
+++ Basic_React_Series/18-routing-advance/src/pages/Product.jsx
@@ -2,8 +2,8 @@ const Product = () => {
   return (
     <div className="flex justify-center items-center">
       <h1 className="text-4xl underline font-bold">This is Product Page</h1>
-    </div>
-  )
+    </div> 
+  )   
 };  
 
 export default Product;
