48112d4de0118bca755d35bf02b5271308c918cf
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-01 12:05:16 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/pages/About.jsx
+++ Basic_React_Series/18-routing-advance/src/pages/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
     return (
         <div>
-            <h1 className=''>This is About Page</h1>.  
+            <h1 className='ext-4xl underline'>This is About Page</h1>.  
         </div>
     )
 };
