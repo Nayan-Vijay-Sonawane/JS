35541b4e4e838c9372021fec81839bf541621df9
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-02 10:17:19 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/pages/Home.jsx
+++ Basic_React_Series/18-routing-advance/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
-        <div className="relative size-32">
-            <h1 className='absolute inset-0 text-4xl underline font-bold'>Home Page</h1>
+        <div className="relative h-screen flex justify-center items-center">
+            <h1 className='absolute text-3xl underline font-bold'>Home Page</h1>
         </div>   
     )
 };  
