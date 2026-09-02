5f57c1607d2cad92621e894d90c56daf9eb2429b
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-02 13:06:39 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/components/Navbar.jsx
+++ Basic_React_Series/18-routing-advance/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <div className="py-5 px-8 bg-cyan-950 flex justify-between items-center font-semibold ">
             <h2 className="text-xl font-bold">Furn-Tech</h2>
             <div className="flex gap-8">
-                <a className="text-lg font-bold" href="/">Home</a>
-                <a className="text-lg font-bold" href="/about">About</a>
-                <a className="text-lg font-bold" href="/product">Product</a>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/product'>Product</Link>
             </div>
         </div>      
     )   
