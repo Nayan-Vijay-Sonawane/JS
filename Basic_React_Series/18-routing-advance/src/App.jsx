88c261b234ef4a17a658515f685da35679b958f8
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-02 10:37:35 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/App.jsx
+++ Basic_React_Series/18-routing-advance/src/App.jsx
@@ -5,6 +5,8 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Product from './pages/Product'
+import NotFound from './pages/NotFound'
+import Men from './pages/Men'
   
 function App() {
 
@@ -15,6 +17,8 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/product' element={<Product />} />
+        <Route path='/product/men' element={<Men />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
