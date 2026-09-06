f903ba080864dfca1d13a42ca5139fa2edc35509
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-06 11:45:27 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/pages/Product.jsx
+++ Basic_React_Series/18-routing-advance/src/pages/Product.jsx
@@ -4,8 +4,8 @@ const Product = () => {
   return (
     <div>
       <div className="flex justify-center gap-10 py-4">
-        <Link className="text-xl font-semibold" to='men'>Men</Link>
-        <Link className="text-xl font-semibold" to='women'>Women</Link>
+        <Link className="text-xl font-semibold" to='/product/men'>Men</Link>
+        <Link className="text-xl font-semibold" to='/product/women'>Women</Link>
       </div>
       <h1 className="font-bold">Product Page</h1>
     </div>  
