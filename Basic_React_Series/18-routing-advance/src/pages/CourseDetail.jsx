f9b9665dd0172a49b435326b897d52721ce9caaf
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-07 12:23:26 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/pages/CourseDetail.jsx
+++ Basic_React_Series/18-routing-advance/src/pages/CourseDetail.jsx
@@ -1,3 +1,5 @@
+import 
+
 const CourseDetail = () => {
     return (
         <div>
--- Basic_React_Series/18-routing-advance/src/pages/Courses.jsx
+++ Basic_React_Series/18-routing-advance/src/pages/Courses.jsx
@@ -1,4 +1,6 @@
+
 const Courses = () => {
+
     return (
         <div>
             <h1 className='font-bold'>Courses Page</h1>
