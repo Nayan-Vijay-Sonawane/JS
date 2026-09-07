130d7bc464ab7d1146e5f47b8a5b2dc323b923d5
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-07 12:38:43 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/pages/About.jsx
+++ Basic_React_Series/18-routing-advance/src/pages/About.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 const About = () => {
+
+    const navigate = useNavigate();
+    const btnClicked = () => {
+        navigate('/');
+    }
+
     return (
         <div>
-            <button className='bg-emerald-800 px-4 py-2 m-2 cursor-pointer active:scale-95 rounded'>Return to Home Page</button>
+            <button onClick={btnClicked} className='bg-emerald-800 px-4 py-2 m-2 cursor-pointer active:scale-95 rounded'>Return to Home Page</button>
             <h1 className='font-bold'>About Page</h1>
         </div>
     )
