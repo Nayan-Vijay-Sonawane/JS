f0b7b8e373f764b6153438071317b00742fac431
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-07 12:32:00 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/pages/About.jsx
+++ Basic_React_Series/18-routing-advance/src/pages/About.jsx
@@ -1,6 +1,7 @@
 const About = () => {
     return (
         <div>
+            <button className='bg-emerald-800 px-4 py-2 m-2 cursor-pointer active:scale-95 rounded'>Return to Home Page</button>
             <h1 className='font-bold'>About Page</h1>
         </div>
     )
