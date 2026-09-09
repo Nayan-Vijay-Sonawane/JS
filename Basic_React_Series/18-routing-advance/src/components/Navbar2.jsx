db7bb4b1b23a8679b22ca528cbb8e31720c5854b
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-09 06:05:03 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/components/Navbar2.jsx
+++ Basic_React_Series/18-routing-advance/src/components/Navbar2.jsx
@@ -18,7 +18,7 @@ const Navbar2 = () => {
             </button>
             <button onClick={() => {
                 navigate(+1)
-            }} className='bg-emerald-800 px-4 py-2 m-2 cursor-pointer active:scale-95'>
+            }} className='bg-emerald-800 px-4 py-2 m-2 cursor-pointer active:scale-95 rounded'>
                 Next
             </button>
         </div>
