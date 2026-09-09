8dd335ee6c2b6fd92a759df8464c4a9438bdff7e
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-09 06:03:18 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/components/Navbar2.jsx
+++ Basic_React_Series/18-routing-advance/src/components/Navbar2.jsx
@@ -16,6 +16,11 @@ const Navbar2 = () => {
             }} className='bg-emerald-800 px-4 py-2 m-2 cursor-pointer active:scale-95 rounded'>
                 Back
             </button>
+            <button onClick={() => {
+                navigate(+1)
+            }} >
+                Next
+            </button>
         </div>
     )
 }
