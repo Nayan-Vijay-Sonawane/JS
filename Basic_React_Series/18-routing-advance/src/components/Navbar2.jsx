4894fde6fd2f0d88e2a9afe42b6a4e70eb8b4157
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-09 05:47:28 +0000

    New react series with new projects going with routing with advance routing

--- Basic_React_Series/18-routing-advance/src/components/Navbar2.jsx
+++ Basic_React_Series/18-routing-advance/src/components/Navbar2.jsx
@@ -1,8 +1,22 @@
-
+import { useNavigate } from 'react-router-dom';
 
 const Navbar2 = () => {
+
+    const navigate = useNavigate();
+
     return (
-        <div></div>
+        <div>
+            <button onClick={() => {
+                navigate('/')
+            }} className='bg-emerald-800 px-4 py-2 m-2 cursor-pointer active:scale-95 rounded'>
+                Return to Home Page
+            </button>
+            <button onClick={() => {
+                navigate(-1)
+            }} className='bg-emerald-800 px-4 py-2 m-2 cursor-pointer active:scale-95 rounded'>
+                Back
+            </button>
+        </div>
     )
 }
 
--- Basic_React_Series/18-routing-advance/src/pages/About.jsx
+++ Basic_React_Series/18-routing-advance/src/pages/About.jsx
@@ -6,16 +6,6 @@ const About = () => {
 
     return (
         <div>
-            <button onClick={() => {
-                navigate('/')
-            }} className='bg-emerald-800 px-4 py-2 m-2 cursor-pointer active:scale-95 rounded'>
-                Return to Home Page
-            </button>
-            <button onClick={() => {
-                navigate(-1)
-            }} className='bg-emerald-800 px-4 py-2 m-2 cursor-pointer active:scale-95 rounded'>
-                Back
-            </button>
             <h1 className='font-bold'>About Page</h1>
         </div>
     )
