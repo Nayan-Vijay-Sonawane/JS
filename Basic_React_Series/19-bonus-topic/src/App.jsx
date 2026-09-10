96544c53b7c63392a6f192a2b21e72aeabdd24ed
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-10 12:19:17 +0000

    New react series with new projects going with a bonus topic of props drilling

--- Basic_React_Series/19-bonus-topic/src/App.jsx
+++ Basic_React_Series/19-bonus-topic/src/App.jsx
@@ -2,11 +2,11 @@ import './App.css'
 import { useState } from 'react';
 
 function App() {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState('dark')
 
   return (
     <div>
-      <h1>Theme is light</h1>
+      <h1>Theme is {theme}</h1>
     </div>
   )
 }
