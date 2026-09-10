0cb9f23d4cbaaa5b4c756471bf736b628b8c5a14
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-10 12:31:51 +0000

    New react series with new projects going with a bonus topic of props drilling

--- Basic_React_Series/19-bonus-topic/src/App.jsx
+++ Basic_React_Series/19-bonus-topic/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { useState } from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   const [theme, setTheme] = useState('light')
@@ -7,6 +8,8 @@ function App() {
   return (
     <div>
       <h1>Theme is {theme}</h1>
+
+      <Navbar theme={theme} />
     </div>
   )
 }
