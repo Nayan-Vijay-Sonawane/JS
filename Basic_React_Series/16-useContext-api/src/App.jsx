fc01bfb253ea306c876ff56f932992fe56ca3da0
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-11 09:38:21 +0000

    New react series with new projects going with a context with useContext-api

--- Basic_React_Series/16-useContext-api/src/App.jsx
+++ Basic_React_Series/16-useContext-api/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Navbar2 from './components/Navbar2'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <Navbar />
+      <Navbar2 />
     </div>
   )
 }
