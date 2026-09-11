15a12469b1d808dc3c382b4fc622f8e398b5c693
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-11 08:53:32 +0000

    New react series with new projects going with a bonus topic of props drilling

--- Basic_React_Series/19-bonus-topic/src/components/Navbar.jsx
+++ Basic_React_Series/19-bonus-topic/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = (props) => {
 
     return (
         <div className="m-5">
-            <button className='px-5 py-2 bg-cyan-950 text-white rounded active:scale-95 cursor-pointer' onClick={changeTheme}>Change theme</button>
+            <button className='px-5 py-2 bg-cyan-950 text-white rounded cursor-pointer active:scale-95' onClick={changeTheme}>Change theme</button>
         </div>
     )
 };
