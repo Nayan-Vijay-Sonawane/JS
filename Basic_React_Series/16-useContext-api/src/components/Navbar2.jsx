31c740849048e8c86a364af87df3cabac0eb61d0
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-11 09:38:00 +0000

    New react series with new projects going with a context with useContext-api

---
+++ Basic_React_Series/16-useContext-api/src/components/Navbar2.jsx
@@ -0,0 +1,12 @@
+const Navbar2 = () => {
+    return (
+        <div>
+            <h4>Home</h4>
+            <h4>About</h4>
+            <h4>Contact</h4>
+            <h4>Product</h4>
+        </div>
+    )
+};
+
+export default Navbar2;
