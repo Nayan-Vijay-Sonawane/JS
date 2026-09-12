9b8ae62e9c8592dbfe7ab0b4502201c91e33c427
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-12 12:38:10 +0000

    New react series with new projects going with a context with useContext-api

--- Basic_React_Series/16-useContext-api/src/components/Navbar2.jsx
+++ Basic_React_Series/16-useContext-api/src/components/Navbar2.jsx
@@ -4,7 +4,7 @@ const Navbar2 = () => {
             <h4>Home</h4>
             <h4>About</h4>
             <h4>Contact</h4>
-            <h4>Product</h4>
+            <h4>Services</h4>
         </div>
     ) 
 };
