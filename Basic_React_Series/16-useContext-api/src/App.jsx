ca3317ac385c5c530250ae66688fcb560256ab5a
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-12 11:56:34 +0000

    New react series with new projects going with a context with useContext-api

--- Basic_React_Series/16-useContext-api/src/App.jsx
+++ Basic_React_Series/16-useContext-api/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Navbar2 from './components/Navbar2'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='h-screen flex justify-between px-5 py-2'>
