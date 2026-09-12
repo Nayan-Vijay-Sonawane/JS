1882722005e4efc80ae8fb056f38ac0fe7cb6c27
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-12 12:19:15 +0000

    New react series with new projects going with a context with useContext-api

--- Basic_React_Series/16-useContext-api/src/App.jsx
+++ Basic_React_Series/16-useContext-api/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import Navbar2 from './components/Navbar2'
 
 function App() {
 
   return (
     <div className='h-screen flex justify-between px-8 py-3 bg-cyan-950'>
       <Navbar />
-      <Navbar2 />
     </div>
   )
 }
--- Basic_React_Series/16-useContext-api/src/components/Navbar.jsx
+++ Basic_React_Series/16-useContext-api/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import Navbar2 from "./Navbar2";
+
 const Navbar = () => {
     return (
-        <div>
+        <div className="flex justify-between ">
             <h1 className="text-3xl font-bold">Furn-tech</h1>
+            <Navbar2 />
         </div>
     )
 };
