72fce0fcb605b87d941cb46d171aba523c896a66
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-14 09:45:31 +0000

    New react series with new projects going with a context with useContext-api

--- Basic_React_Series/16-Context-api/src/components/Navbar.jsx
+++ Basic_React_Series/16-Context-api/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import Navbar2 from "./Navbar2";
 import { useContext } from 'react'
+import ThemeDataContext from '../context/ThemeDataContext'
 
 const Navbar = (props) => {
     
     useContext(ThemeDataContext);
-    
+
     return (
         <div className="flex justify-between items-center px-5 py-4 bg-cyan-800 text-white">
             <h1 className="text-3xl font-bold">Furn-tech</h1>
