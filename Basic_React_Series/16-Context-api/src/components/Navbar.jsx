9808e4760b9b17965e31f87685857a97a94ed2a1
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-17 13:43:47 +0000

    New react series with new projects going with a context with useContext-api

--- Basic_React_Series/16-Context-api/src/components/Navbar.jsx
+++ Basic_React_Series/16-Context-api/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Navbar2 from "./Navbar2";
-import { useContext } from 'react'
-import {ThemeDataContext} from '../context/ThemeContext'
+// import { useContext } from 'react'
+// import {ThemeDataContext} from '../context/ThemeContext'
 
 const Navbar = (props) => {
     
