9665d98c717b4d2d4320770f4a689644540ffe7e
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-18 13:11:22 +0000

    New react series with new projects going with a context with useContext-api

--- Basic_React_Series/16-Context-api/src/context/ThemeContext.jsx
+++ Basic_React_Series/16-Context-api/src/context/ThemeContext.jsx
@@ -6,7 +6,7 @@ const ThemeContext = (props) => {
     return (
         <div>
             <ThemeDataContext.Provider>
-                
+                {props.Children}
             </ThemeDataContext.Provider>
         </div>
     )
