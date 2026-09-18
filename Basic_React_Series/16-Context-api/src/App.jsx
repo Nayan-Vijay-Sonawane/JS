dc3c8fc09409395dde33fdf92991126bc967621c
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-18 13:12:10 +0000

    New react series with new projects going with a context with useContext-api

--- Basic_React_Series/16-Context-api/src/App.jsx
+++ Basic_React_Series/16-Context-api/src/App.jsx
@@ -2,9 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
  
 function App() {
-
-  const [theme, setTheme] = useState('Light');
-
+  
   return (
     <div className='h-screen bg-cyan-950'>
       <Navbar theme={theme}>
