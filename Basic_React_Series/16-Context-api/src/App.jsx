178dd75297cb76fd86865b2bd6d54bce34218dc9
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-21 16:55:27 +0000

    New react series with new projects going with a context with useContext-api

--- Basic_React_Series/16-Context-api/src/App.jsx
+++ Basic_React_Series/16-Context-api/src/App.jsx
@@ -1,8 +1,8 @@
-import './App.css';
+import './App.css'; 
 import Navbar from './components/Navbar';
  
 function App() {
-  
+
   return (
     <div className='h-screen bg-cyan-950'>
       <Navbar /> 
