12fb1e1f305b941920a8b911c186276deca91e85
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-23 14:08:43 +0000

    New react series with new projects going with a context with useContext-api

--- Basic_React_Series/16-Context-api/src/App.jsx
+++ Basic_React_Series/16-Context-api/src/App.jsx
@@ -1,13 +1,19 @@
+import { useContext } from 'react';
 import './App.css'; 
 import Button from './components/Button';
 import Navbar from './components/Navbar';
+import { ThemeDataContext } from './context/ThemeContext';
  
 function App() {
 
+  const [theme] = useContext(ThemeDataContext)
+
   return (
     <div className='h-screen bg-cyan-950'>
-      <Navbar /> 
-      <Button />
+      <div className={theme}>
+        <Navbar />
+        <Button />
+      </div>
     </div>
   )
 };
