6aaf75d730f67d77ffc2de171b9ac93eaadc4eaa
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-09-23 13:48:14 +0000

    New react series with new projects going with a context with useContext-api

--- Basic_React_Series/16-Context-api/src/components/Button.jsx
+++ Basic_React_Series/16-Context-api/src/components/Button.jsx
@@ -1,7 +1,13 @@
 const Button = () => {
+
+    const changeTheme = () => {
+        console.log("Hello");
+        
+    }
+
     return (
        <div>
-        <button className="m-5 px-5 py-2 bg-cyan-500 rounded active:scale-95 cursor-pointer">Change the Theme</button>
+        <button onClick={} className="m-5 px-5 py-2 bg-cyan-500 rounded active:scale-95 cursor-pointer">Change the Theme</button>
        </div> 
     )
 };
